e3a27a01bea85f79aa82e56d36b724b873c69d1d
Akshay Masram <akshaysunilmasram@yahoo.com>
2023-10-08 17:44:19 +0530

    Update sign_up_screen.dart

--- lib/sign_up_screen.dart
+++ lib/sign_up_screen.dart
@@ -12,7 +12,8 @@ class SignUpScreen extends StatefulWidget {
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return const Scaffold(
+
+    );
   }
 }
