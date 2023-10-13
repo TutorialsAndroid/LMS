6011f857156ad9432dc227497da4e1b667bd17eb
Akshay Masram <akshaysunilmasram@yahoo.com>
2023-10-13 14:51:52 +0530

    Added Banned ads in home screen

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -37,15 +37,29 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(6),
             child: Text(
               'Master of Computer Applications (Online Mode) JAN, 2022',
               style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+            ),
+          ),
+
+          Card(
+            elevation: 4, // Controls the shadow and depth of the card
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)), // Controls the shape of the card
+            color: Colors.white, // Background color of the card
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(1.0),
+                  child: Image.asset('assets/navratri_lms.jpg')
+                ),
+              ],
             ),
           )
         ],
