d0b549c6a8e3e62dfb09b47008b3e4c490086a9c
Akshay Masram <akshaysunilmasram@yahoo.com>
2023-10-14 15:12:04 +0530

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -113,7 +113,7 @@ class _HomeScreenState extends State<HomeScreen> {
               ),
             ),
 
-            MyCalendarWidget(),
+            const MyCalendarWidget(),
           ],
         ),
       )
