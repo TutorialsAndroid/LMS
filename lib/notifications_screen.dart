187c73e25c61fb160aefc42deb6c4731407c7337
Akshay Masram <akshaysunilmasram@yahoo.com>
2023-10-16 15:41:35 +0530

    Create notifications_screen.dart

---
+++ lib/notifications_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class NotificationsScreen extends StatefulWidget {
+  const NotificationsScreen({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<NotificationsScreen> createState() => _NotificationsScreenState();
+}
+
+class _NotificationsScreenState extends State<NotificationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
