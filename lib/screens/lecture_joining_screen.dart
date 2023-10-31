413fa9935d9d60711e54ef4ad562fa14dcdc8abe
Akshay Masram <akshaysunilmasram@yahoo.com>
2023-11-01 00:28:36 +0530

    Added bottom sheet in lecture joining UI screen

--- lib/screens/lecture_joining_screen.dart
+++ lib/screens/lecture_joining_screen.dart
@@ -9,9 +9,119 @@ class LectureJoining extends StatefulWidget {
 }
 
 class _LectureJoiningScreenState extends State<LectureJoining> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _showBottomSheet() {
+    _scaffoldKey.currentState?.showBottomSheet( (context) {
+        return Container(
+          height: 200,
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('assets/profile_photo/t.png'), // Provide the path to your image
+                            radius: 50, // Set the radius of the circle
+                          ),
+                        ),
+
+                        SizedBox(width: 10,),
+
+                        Text('Tushar')
+                      ],
+                    ),
+                  ),
+
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('assets/profile_photo/a.png'), // Provide the path to your image
+                            radius: 50, // Set the radius of the circle
+                          ),
+                        ),
+
+                        SizedBox(width: 10,),
+
+                        Text('Akshay')
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('assets/profile_photo/j.png'), // Provide the path to your image
+                            radius: 50, // Set the radius of the circle
+                          ),
+                        ),
+
+                        SizedBox(width: 10,),
+
+                        Text('Jaydeep')
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('assets/profile_photo/s.png'), // Provide the path to your image
+                            radius: 50, // Set the radius of the circle
+                          ),
+                        ),
+
+                        SizedBox(width: 10,),
+
+                        Text('Surabh')
+                      ],
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          child: CircleAvatar(
+                            backgroundImage: AssetImage('assets/profile_photo/s.png'), // Provide the path to your image
+                            radius: 50, // Set the radius of the circle
+                          ),
+                        ),
+
+                        SizedBox(width: 10,),
+
+                        Text('Sushant')
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       backgroundColor: const Color(0xFF000000),
       appBar: AppBar(
         backgroundColor: const Color(0xFF000000),
@@ -49,7 +159,7 @@ class _LectureJoiningScreenState extends State<LectureJoining> {
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.mic_off),
             label: 'Unmute',
@@ -60,13 +170,18 @@ class _LectureJoiningScreenState extends State<LectureJoining> {
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.group),
-            label: 'Participants',
+            label: 'Participants 5',
           ),
         ],
         backgroundColor: Colors.black12,
         selectedItemColor: Colors.white70,
         unselectedItemColor: Colors.white70,
-
+        onTap: (index) {
+          if (index == 2) {
+            // Show the bottom sheet when the "More" tab is clicked
+            _showBottomSheet();
+          } else {}
+        }
       ),
     );
   }
